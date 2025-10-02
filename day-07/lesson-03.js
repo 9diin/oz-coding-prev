e335348b37ac554a3b27d165b1c9d7ba7b1a0576
9Diin <weartstudio@naver.com>
2025-10-02 14:39:28 +0900

    오즈코딩스쿨 - HTML 강의자료 7일차

--- day-07/lesson-03.js
+++ day-07/lesson-03.js
@@ -28,3 +28,90 @@ console.log(sum); // 45
 // 배열 요소의 경우, 첫 번째에서 시작해 마지막으로 순회합니다.
 // 배열은 '동적'으로 순회하기 때문에 즉, 반복 중간에 배열 자체에 변화가 발생한다면 반복 결과가 바뀔 수도 있으니
 // 이 점은 유의하셔야 합니다.
+
+// for-of와 참조 타입의 객체
+// 참조 타입의 객체는 기본적으로 이터러블이 아닙니다.
+// 일반적인 객체에 for-of를 사용하려 한다면 런타임에 TypeError가 발생합니다.
+
+let obj = {
+    x: 1,
+    y: 2,
+    z: 3,
+};
+
+// for (let element of obj) {
+// statement
+// console.log("element: ", element); // Uncaught TypeError: obj is not iterable
+// }
+
+// 참조 타입의 객체의 프로퍼티를 순회하고 싶다면, for-in 루프를 사용하거나 Object.keys() 메서드를 사용하여
+// 키 값으로 이루어진 배열을 반환받아 해당 배열을 통해 for-of를 사용해야 합니다.
+
+// [참고] 키를 순회하는 것은 동적이지 않습니다.
+// Object.keys()는 객체의 프로퍼티 이름으로 이루어진 배열을 반환하며 배열은 for-of 루프를 사용할 수 있는 이터러블 객체이잖아요?
+
+// 키는 문자열 또는 심벌이기 때문에 Object.keys() 메서드를 통해 키 값으로 이루어진 배열의 모든 요소는 String 입니다.
+// console.log(Object.keys(obj)); // ['x', 'y', 'z']
+
+let total1 = 0;
+let total2 = 0;
+
+for (let element of Object.keys(obj)) {
+    // obj.x === obj["x"];
+    // obj.y === obj["y"];
+    // obj.z === obj["z"];
+
+    // statement
+    // 객체의 대괄호 표기법을 사용
+    console.log("element: ", element); // "x" or "y" or "z"
+    console.log("obj[element]: ", obj[element]); // 1 or 2 or 3
+    // console.log("obj.element: ", obj.element); // undefined
+
+    total1 = total1 + obj[element];
+}
+console.log(total1); // 6
+
+for (let value of Object.values(obj)) {
+    total2 = total2 + value;
+}
+console.log(total2); // 6
+
+// 객체 프로퍼티의 키와 값이 모두 필요하다면, Object.entries()와 분해 할당을 통해 for-of도 사용할 수 있습니다.
+for (let [key, value] of Object.entries(obj)) {
+    // statement
+    console.log(Object.entries(obj)); // [["x", 1], ["y", 2], ["z", 3]]
+    // console.log(element); // ['x', 1] or ["y", 2] or ["z", 3]
+    console.log("key: ", key, "value: ", value);
+}
+
+// 5. for-in
+// for-in 루프는 of 키워드가 in으로 바뀐 점을 제외하면 for-of 루프와 거의 비슷합니다.
+// for-of 루프는 of 다음에 이터러블 객체가 와야 하지만, for-in 루프는 in 다음에 어떤 객체든 쓸 수 있습니다.
+
+// for (variable in object) {
+//     statement;
+// }
+
+// variable에는 일반적으로 변수 이름이 들어가지만 for-of와 마찬가지로 변수 선언이 될 수도 있고
+// 할당 표현식에서 왼쪽에 올 수 있는 것은 무엇이든 가능합니다.
+// object는 객체로 평가되는 표현식입니다.
+// 다른 문과 마찬가지로 statement는 루프 바디 구실을 하는 문 또는 문 블록입니다.
+
+let object = {
+    user1: "김아무개",
+    user2: "이아무개",
+    user3: "박아무개",
+};
+
+for (let property in object) {
+    // statement
+    // object의 프로퍼티 이름을 변수 property에 할당합니다.
+    console.log(property); // user1 or user2 or user3
+    console.log(object[property]); // 각 프로퍼티 값을 출력합니다.
+}
+
+// 자바스크립트 인터프리터는 for-in 문을 실행할 때 첫 번째로 object 표현식을 평가합니다.
+// 그 표현식이 null이나 undefined로 평가되면 인터프리터는 루프를 건너뛰고 다음 문으로 이동합니다.
+
+// 제가 직전 강의에서 for-of는 배열에서, for-in 객체에서 사용한다고 말씀드렸는데요.
+// 이렇게 딱 잘라 말씀드린 이유는 해당 루프들은 위 경우에 많이 사용되기 때문입니다.
